2468451e5894a3a9e0b7586d32ce9b53ca282032
jusmrcrd <ricardo.m.gama@hotmail.com>
2024-08-08 23:47:53 -0300

    first

---
+++ src/test/java/Payment/SimplePaymentAPI/SimplePaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Payment.SimplePaymentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
